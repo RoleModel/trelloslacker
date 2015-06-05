2f68d926b09997a84c51bf48fc2f48130f398643
Caleb Woods <calebawoods@gmail.com>
2015-06-05 14:48:23 -0400

    Initial commit
    Post cards to Trello after receiving from Slack

---
+++ env.rb
@@ -0,0 +1,7 @@
+# Local override
+dotenv = File.expand_path("../.env_overrides.rb", __FILE__)
+require dotenv if File.exist?(dotenv)
+
+ENV["TRELLO_DEVELOPER_PUBLIC_KEY"] ||= ""
+ENV["TRELLO_MEMBER_TOKEN"] ||= ""
+ENV["SLACK_TOKEN"] ||= ""
---
+++ server.rb
@@ -0,0 +1,35 @@
+require 'bundler'
+Bundler.setup
+
+require_relative 'env'
+
+require 'ostruct'
+require 'date'
+require 'pry'
+require 'trello'
+require 'sinatra'
+
+Trello.configure do |config|
+  config.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
+  config.member_token = ENV['TRELLO_MEMBER_TOKEN']
+end
+
+TrelloConfig = Struct.new(:board, :list)
+
+board_mapper = {
+  'trello' => TrelloConfig.new('ujuCZb8i', 'Proposed Ideas')
+}
+
+post '/cards' do
+  config = board_mapper[params[:channel_name]]
+  board = Trello::Board.find(config.board)
+  lists = board.lists
+
+  list = lists.detect { |l| l.name == config.list }
+
+  card_name = params[:text].gsub('add ', '')
+
+  card = Trello::Card.create({ name: card_name, list_id: list.id, pos: 'bottom' })
+
+  "Created the card #{card.name} (#{card.url}) on #{board.name} > #{list.name}"
+end
